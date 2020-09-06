9f5f110325da859f9c0f38177232132fc315b6a0
bluesharkboy <cherrymobileq6i@gmail.com>
2020-09-06 18:35:39 +0800

    Update: gcnmagdaleno Gian Fix-019

--- smooth_scrolling.js
+++ smooth_scrolling.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    $("a ").on("click ", function(a) {
-        if (this.hash !== " ") {
-            a.preventDefault();
-            var b = this.hash;
-            $("html, body ").animate({
-                    scrollTop: $(b).offset().top,
-                },
-                200,
-                function() {
-                    window.location.hash = b;
-                }
-            );
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
         }
     });
 });
