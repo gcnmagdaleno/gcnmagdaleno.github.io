8c5c93d2f96c23efef348dca49bb8ae93e49fd9b
bluesharkboy <cherrymobileq6i@gmail.com>
2020-09-24 16:54:18 +0800

    Update: gcnmagdaleno Gian Fix-030

--- cursor.js
+++ cursor.js
@@ -1,27 +1,41 @@
-let mouseCursor = document.querySelector('.cursor');
-let iframe = document.querySelector('iframe');
-let map = document.querySelector('#map');
+let mouseCursor = document.querySelector(".cursor");
+let htmlSelector = document.querySelector("*");
+let iframe = document.querySelector("iframe");
+let map = document.querySelector("#map");
 
-window.addEventListener('mousemove', cursor);
+// window.addEventListener("mousemove", cursor);
 
-function cursor(e) {
-    // console.log(e);
-    mouseCursor.style.top = e.pageY + 'px';
-    mouseCursor.style.left = e.pageX + 'px';
-}
+// function cursor(e) {
+//     // console.log(e);
+//     mouseCursor.style.top = e.clientY;
+//     mouseCursor.style.left = e.clientX;
+// }
+
+$(document).on("mousemove", function(e) {
+    $(".cursor").css({
+        left: e.clientX,
+        top: e.clientY,
+    });
+    // $(".cursor").css("display:block;");
+    // mouseCursor.style.display = "block";
+});
+
+htmlSelector.addEventListener("mousemove", function() {
+    mouseCursor.style.display = "block";
+});
 
 iframe.addEventListener("mouseover", function() {
-    mouseCursor.style.display = 'none';
-})
+    mouseCursor.style.display = "none";
+});
 
 iframe.addEventListener("mouseleave", function() {
-    mouseCursor.style.display = 'block';
-})
+    mouseCursor.style.display = "block";
+});
 
 map.addEventListener("mouseover", function() {
-    mouseCursor.style.display = 'none';
-})
+    mouseCursor.style.display = "none";
+});
 
 map.addEventListener("mouseleave", function() {
-    mouseCursor.style.display = 'block';
-})
+    mouseCursor.style.display = "block";
+});
