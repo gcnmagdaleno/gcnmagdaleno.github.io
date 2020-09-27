ae93cdd3e4523941e13b0f0257ec33ebaf22f777
bluesharkboy <cherrymobileq6i@gmail.com>
2020-09-27 14:30:31 +0800

    Update: gcnmagdaleno Gian Fix-033

--- cursor.js
+++ cursor.js
@@ -10,12 +10,40 @@ let map = document.querySelector("#map");
 //     mouseCursor.style.top = e.clientY;
 //     mouseCursor.style.left = e.clientX;
 // }
-
+let timer = 0;
 $(document).on("mousemove", function(e) {
+    $('.cursor').show();
     $(".cursor").css({
         left: e.clientX,
         top: e.clientY,
     });
+
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+        // hides the cursor when the user stops moving the mouse after 4.555 seconds
+        $('.cursor').hide();
+    }, 2555);
+
+    console.log('timer: ' + timer);
+
+    console.log(e);
+
+
+    console.log('clientX' + e.screenX);
+
+    console.log('pageX' + e.pageX);
+
+
+    console.log('X' + e.X);
+
+    // $(document).hover(function() {
+    //     console.log('hovered');
+    // });
+
+    let hovered = $('html:hover,body:hover,html:active,body:active').length > 0 ? 'hovered' : 'not hovered';
+    console.log(hovered);
+
+
     // $(".cursor").css("display:block;");
     // mouseCursor.style.display = "block";
 });
